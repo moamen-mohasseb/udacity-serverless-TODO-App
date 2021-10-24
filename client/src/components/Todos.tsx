add61be89466bea7b9cd293c1df1f7c0d0424fac
moamen_mohasseb <moamen_mohasseb@yahoo.com>
2021-10-24 10:02:40 +0200

    ready for second review2

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -47,7 +47,6 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
   onTodoCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
     try {
       if(this.state.newTodoName.length>0){
-        alert(`Todo Update failed ${this.state.newTodoName}`)
       const dueDate = this.calculateDueDate()
       const newTodo = await createTodo(this.props.auth.getIdToken(), {
         name: this.state.newTodoName,
